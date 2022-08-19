44249e4c015831bd9d5db5dac52e60abf6cbe192
AnthonyLimanto <82982657+AnthonyLimanto@users.noreply.github.com>
2022-08-19 18:01:02 +1000

    base code

---
+++ SEPT_Project/src/main/java/com/sfr/sept_project/SeptProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sfr.sept_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeptProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeptProjectApplication.class, args);
+    }
+
+}
