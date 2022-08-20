6da9a809710493288073d562e3706b035feceed2
AnthonyLimanto <82982657+AnthonyLimanto@users.noreply.github.com>
2022-08-20 11:53:26 +1000

    base code

---
+++ SFR_Project/src/main/java/com/example/sfr_project/SfrProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.sfr_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfrProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfrProjectApplication.class, args);
+    }
+
+}
---
+++ SFR_Project/src/test/java/com/example/sfr_project/SfrProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sfr_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfrProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
