24fd33b20eb0f39233a43a712c6c5797fb66adca
AnthonyLimanto <82982657+AnthonyLimanto@users.noreply.github.com>
2022-08-20 11:43:25 +1000

    base code

---
+++ SEPT_Project/src/test/java/com/example/sept_project/SeptProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sept_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeptProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
