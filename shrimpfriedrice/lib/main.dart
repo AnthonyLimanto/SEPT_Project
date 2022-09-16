76ce1a614cc5c5e7908fa8e26f2ee6a98024565f
b-ben-03 <s3906302@student.rmit.edu.au>
2022-09-16 11:50:30 +1000

    Start Frontend Appointment Form

--- shrimpfriedrice/lib/main.dart
+++ shrimpfriedrice/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:shrimpfriedrice/Pages/home.dart';
 import 'package:shrimpfriedrice/Pages/login.dart';
 import 'package:shrimpfriedrice/Pages/register.dart';
+import 'package:shrimpfriedrice/Pages/appointment_form';
 
 void main() {
   runApp(
@@ -12,6 +13,7 @@ void main() {
           'home': (context) => const Home(),
           'login': (context) => const Login(),
           'register': (context) => const Register(),
+          'book': (context) => const AppointmentForm(),
         }),
   );
 }
