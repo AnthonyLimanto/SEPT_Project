904eeac0aca8865adc494eeaca22d76a1a9581a5
b-ben-03 <s3906302@student.rmit.edu.au>
2022-09-20 20:07:28 +1000

    Fix code formatting/indentations

--- shrimpfriedrice/lib/Pages/home.dart
+++ shrimpfriedrice/lib/Pages/home.dart
@@ -9,62 +9,67 @@ class Home extends StatefulWidget {
 }
 
 class HomeState extends State<Home> {
-  String name = 'Ben';
+  String name = '<User>';
   final textButtonStyle =
       TextButton.styleFrom(padding: const EdgeInsets.all(20.0));
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: ListView(padding: const EdgeInsets.all(50.0), children: <Widget>[
-      Center(
-          child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 30.0),
-              child: Text("Welcome, $name Book your next appointment.",
-                  style: const TextStyle(
-                      fontSize: 28, fontWeight: FontWeight.bold)))),
-      const AppointmentList(),
-      TextButton(
-        style: textButtonStyle,
-        onPressed: () {
-          Navigator.pushNamed(context, 'medication');
-        },
-        child: const Text("My medication"),
+      body: ListView(padding: const EdgeInsets.all(50.0), children: <Widget>[
+          Center(
+              child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 30.0),
+                  child: Text("Welcome, $name Book your next appointment.",
+                      style: const TextStyle(
+                          fontSize: 28, fontWeight: FontWeight.bold),
+                  ),
+              ),
+          ),
+          const AppointmentList(),
+          TextButton(
+            style: textButtonStyle,
+            onPressed: () {
+              Navigator.pushNamed(context, 'medication');
+            },
+            child: const Text("My medication"),
+          ),
+          TextButton(
+            style: textButtonStyle,
+            onPressed: () {
+              Navigator.pushNamed(context, 'book');
+            },
+            child: const Text("Book an appointment"),
+          ),
+          TextButton(
+            style: textButtonStyle,
+            onPressed: () {},
+            child: const Text("Chat with a doctor"),
+          ),
+          TextButton(
+            style: textButtonStyle,
+            onPressed: () {},
+            child: const Text("Appointment history"),
+          ),
+          TextButton(
+            style: textButtonStyle,
+            onPressed: () {},
+            child: const Text("Manage appointments"),
+          ),
+          TextButton(
+            style: textButtonStyle,
+            onPressed: () {},
+            child: const Text("Account settings"),
+          ),
+          TextButton(
+            style: textButtonStyle,
+            onPressed: () {
+              Navigator.pushNamed(context, 'login');
+            },
+            child: const Text("Start"),
+          ),
+        ],
       ),
-      TextButton(
-        style: textButtonStyle,
-        onPressed: () {
-          Navigator.pushNamed(context, 'book');
-        },
-        child: const Text("Book an appointment"),
-      ),
-      TextButton(
-        style: textButtonStyle,
-        onPressed: () {},
-        child: const Text("Chat with a doctor"),
-      ),
-      TextButton(
-        style: textButtonStyle,
-        onPressed: () {},
-        child: const Text("Appointment history"),
-      ),
-      TextButton(
-        style: textButtonStyle,
-        onPressed: () {},
-        child: const Text("Manage appointments"),
-      ),
-      TextButton(
-        style: textButtonStyle,
-        onPressed: () {},
-        child: const Text("Account settings"),
-      ),
-      TextButton(
-        style: textButtonStyle,
-        onPressed: () {
-          Navigator.pushNamed(context, 'login');
-        },
-        child: const Text("Start"),
-      ),
-    ]));
+    );
   }
 }
