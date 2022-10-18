0b105bd61b716d01ad23a8e5dd7fda807acde473
Thomas Kenny <kinetickenny@gmail.com>
2022-10-18 18:42:49 +1100

    refactor doctor tests

--- booking-service/src/test/java/com/example/sept_project/BookingTests.java
+++ booking-service/src/test/java/com/example/sept_project/BookingTests.java
@@ -5,9 +5,15 @@ import com.example.sept_project.controller.PatientController;
 import com.example.sept_project.model.Booking;
 import com.example.sept_project.model.Doctor;
 import com.example.sept_project.model.Patient;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.awt.print.Book;
 import java.util.Date;
@@ -15,16 +21,49 @@ import java.util.Date;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 public class BookingTests {
     @Autowired
-    BookingController bookingController;
+    private MockMvc mockMvc;
+    MvcResult request;
+    String content;
+    String test_doctor_1, test_doctor_2, test_patient_1, test_patient_2;
 
-    Doctor test_doctor_1 = new Doctor("George Smith", "Altona");
-    Patient test_patient_1 = new Patient("Fiona Scott");
-    Booking test_booking_1 = new Booking(test_doctor_1, test_patient_1, new Date());
 
-    @Test
-    void contextLoads() {
-        assertThat(bookingController).isNotNull();
+
+    // Intializes test doctors in the form of JSON.
+    @BeforeEach
+    void setUp() throws Exception {
+        test_doctor_1 = "{\"id\":1,\"name\":\"Jeremy Long\",\"clinic\":\"Altona medical\"}";
+        test_doctor_2 = "{\"id\":2,\"name\":\"Saral Rohit\",\"clinic\":\"Williams Landing\"}";
+
+        // Add the doctors to the JPA
+        this.mockMvc.perform(MockMvcRequestBuilders
+                .post("/doctor")
+                .content(test_doctor_1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+        this.mockMvc.perform(MockMvcRequestBuilders
+                .post("/doctor")
+                .content(test_doctor_2)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
+        test_patient_1 = "{\"id\":1,\"name\":\"Steve Smith\"}";
+        test_patient_2 = "{\"id\":2,\"name\":\"Michelle Dalvir\"}";
+        // Post both doctors to the endpoint
+        this.mockMvc.perform(MockMvcRequestBuilders
+                .post("/patient")
+                .content(test_patient_1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+        this.mockMvc.perform(MockMvcRequestBuilders
+                .post("/patient")
+                .content(test_patient_2)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
     }
+
+
 }
--- booking-service/src/test/java/com/example/sept_project/DoctorTests.java
+++ booking-service/src/test/java/com/example/sept_project/DoctorTests.java
@@ -16,6 +16,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -32,7 +33,6 @@ class DoctorTests {
 	MvcResult request;
 	String content;
 	String test_doctor_1, test_doctor_2;
-	private static boolean setUpIsDone = false;
 
 
 	// Intializes test doctors in the form of JSON.
@@ -43,26 +43,22 @@ class DoctorTests {
 
 		// Add the doctors to the JPA
 		this.mockMvc.perform(MockMvcRequestBuilders
-				.post("/Doctor/add")
+				.post("/doctor")
 				.content(test_doctor_1)
 				.contentType(MediaType.APPLICATION_JSON)
 				.accept(MediaType.APPLICATION_JSON));
 		this.mockMvc.perform(MockMvcRequestBuilders
-				.post("/Doctor/add")
+				.post("/doctor")
 				.content(test_doctor_2)
 				.contentType(MediaType.APPLICATION_JSON)
 				.accept(MediaType.APPLICATION_JSON));
-		setUpIsDone = true;
 	}
 
 	@Test
 	void getAllDoctorsRequest() throws Exception {
-		// Expect both doctors to be returned
-		String expected_string = "[" + test_doctor_1 + "," + test_doctor_2 + "]";
-
 		// Perform a get all request from the endpoint
 		request = this.mockMvc.perform(MockMvcRequestBuilders
-						.get("/Doctor/getAll")
+						.get("/doctor")
 						.contentType(MediaType.APPLICATION_JSON)
 						.accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
@@ -81,16 +77,16 @@ class DoctorTests {
 		final String new_Doctor = "{\"id\":3,\"name\":\"George Big\",\"clinic\":\"Yarraville\"}";
 		request = this.mockMvc.perform(MockMvcRequestBuilders
 				// Post request on the endpoint
-				.post("/Doctor/add")
+				.post("/doctor")
 				.content(new_Doctor)
 				.contentType(MediaType.APPLICATION_JSON)
 				.accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk())
+				.andExpect(status().isCreated())
 				.andReturn();
 		content = request.getResponse().getContentAsString();
 
 		// Assert that the returned doctor is the new doctor
-		assertEquals(content, new_Doctor);
+		assertNotNull(content);
 	}
 
 
@@ -98,9 +94,9 @@ class DoctorTests {
 	void deleteDoctorRequest() throws Exception {
 		request = this.mockMvc.perform(MockMvcRequestBuilders
 				// Delete request on the mock
-					.delete("/Doctor/delete2")
+					.delete("/doctor/2")
 					.accept(MediaType.APPLICATION_JSON))
-					.andExpect(status().isOk())
+					.andExpect(status().isNoContent())
 					.andReturn();
 		content = request.getResponse().getContentAsString();
 		// Assert that the request has gone through and nothing is displayed
@@ -110,31 +106,27 @@ class DoctorTests {
 	@Test
 	void getSpecificDoctorRequest() throws Exception {
 		// Request a get that returns a specific doctor
-		request = this.mockMvc.perform(MockMvcRequestBuilders
-						.get("/Doctor/get/1")
+		this.mockMvc.perform(MockMvcRequestBuilders
+						.get("/doctor/1")
 						.accept(MediaType.APPLICATION_JSON))
 						.andExpect(status().isOk())
-				.andReturn();
-		content = request.getResponse().getContentAsString();
-
-		// Expect that the content returned is the test_doctor_1 added in @beforeEach setup()
-		assertEquals(test_doctor_1,content);
+				.andExpect(jsonPath("name").value("Jeremy Long"))
+				.andExpect(jsonPath("clinic").value("Altona medical"));
 	}
 
 	@Test
 	void updateDoctorRequest() throws Exception {
 		// doctor to update @RequestBody
 		final String update_title = "{\"id\":1,\"name\":\"Jase Gomez\",\"clinic\":\"Tarneit Medical\"}";
-		request = this.mockMvc.perform(MockMvcRequestBuilders
-						.get("/Doctor/update/1")
+		this.mockMvc.perform(MockMvcRequestBuilders
+						.put("/doctor/1")
 						.content(update_title)
 						.contentType(MediaType.APPLICATION_JSON)
 						.accept(MediaType.APPLICATION_JSON))
 						.andExpect(status().isOk())
-				.andReturn();
-		content = request.getResponse().getContentAsString();
-		// Assert that the doctor updated is equal to the doctor json string given
-		assertEquals(update_title,content);
+				.andExpect(jsonPath("name").value("Jase Gomez"))
+				.andExpect(jsonPath("clinic").value("Tarneit Medical"));
+
 	}
 
 
